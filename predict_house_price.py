d460a387aaa030061979c5523bd9fb9dc9bfb229
Don Haughton <donhaughton@Dons-MacBook-Air.local>
2024-02-15 20:13:48 +0000

    first pass - shouldnt be using accuracy as this isnt a regression problem

--- predict_house_price.py
+++ predict_house_price.py
@@ -9,7 +9,27 @@ from sklearn.model_selection import train_test_split
 
 
 def house_price_predictor():
-    california_housing = fetch_california_housing()
+    california_housing = fetch_california_housing(as_frame=True)
+
     X = california_housing.data
     y = california_housing.target
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(X_train)
+    x_test = scaler.transform(X_test)
+    model = LinearRegression()
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_train)
+    accuracy = (np.mean(y_pred == y_train) * 100)
+    test_y_pred = model.predict(x_test)
+    test_accuracy = (np.mean(test_y_pred == y_test) * 100)
+    print(f"Training accuracy: {accuracy}")
+    print(f"Test accuracy: {test_accuracy}")
+
+
+
+
+
+if __name__ == '__main__':
+    house_price_predictor()
